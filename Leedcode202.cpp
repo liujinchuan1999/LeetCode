bb871b73f596d43c30bd41441ab627c29b13a757
liujinchuan1999 <838760318@qq.com>
2020-02-28 18:03:44 +0800

    202

---
+++ Leedcode202.cpp
@@ -0,0 +1,19 @@
+nt get_next(int n) {
+    int temp = 0;
+    while (n) {
+        temp += (n % 10) * (n % 10);
+        n /= 10;
+    }
+    return temp;
+}
+
+
+bool isHappy(int n){
+    int p = n, q = n;
+    while (q != 1) {
+        p = get_next(p);
+        q = get_next(get_next(q));
+        if (p == q) break;
+    }
+    return q == 1;
+}
